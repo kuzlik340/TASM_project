1fb664dc9cffb53a52247d39ff52336788dbabd4
kuzlik340 <timkkuz@gmail.com>
2025-02-22 16:45:58 +0100

    first commit

---
+++ hello.asm
@@ -0,0 +1,51 @@
+.model small
+.stack 100h
+
+.data
+    prompt db 'Enter argument: ', '$'
+    hello_msg db 13, 10, 'Hello', 13, 10, '$'
+    bye_msg db 13, 10, 'Bye', 13, 10, '$'
+    buffer db 10 dup('$')  ; buffer for user input
+
+.code
+start:
+    mov ax, @data
+    mov ds, ax
+
+    ; Display prompt
+    mov dx, offset prompt
+    mov ah, 09h
+    int 21h
+
+    ; Read user input
+    lea dx, buffer
+    mov ah, 0Ah
+    int 21h
+
+    ; Compare input with '-h'
+    lea si, buffer + 2     ; Skip input length bytes
+    mov al, [si]           ; First character
+    cmp al, '-'
+    jne print_bye
+
+    mov al, [si+1]         ; Second character
+    cmp al, 'h'
+    jne print_bye
+
+    ; If match, print "Hello"
+    mov dx, offset hello_msg
+    mov ah, 09h
+    int 21h
+    jmp exit_program
+
+print_bye:
+    ; Otherwise, print "Bye"
+    mov dx, offset bye_msg
+    mov ah, 09h
+    int 21h
+
+exit_program:
+    mov ah, 4Ch
+    int 21h
+
+end start
